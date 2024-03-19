60410552d8214e32e8c43fcedaedc311185fca52
Chagit <chagitalxndr@gmail.com>
2024-03-19 23:50:48 +0200

    Fixing issues: -Corrected definition of relationships between tables Patient and Vaccination. -Correcting the driver name

--- Corona-management-system/connect_to_sqlserver.py
+++ Corona-management-system/connect_to_sqlserver.py
@@ -5,8 +5,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 class ConnectSQL:
-    DRIVER_NAME = "SQL SERVER"
-    SERVER_NAME = "DESKTOP-E20A7OU/SQLEXPRESS"
+    DRIVER_NAME = "ODBC Driver 17 for SQL Server"
+    SERVER_NAME = "DESKTOP-E20A7OU\SQLEXPRESS"
     DATABASE_NAME = "HMODatabase"
 
     quoted = urllib.parse.quote_plus(f"DRIVER={DRIVER_NAME};SERVER={SERVER_NAME};DATABASE={DATABASE_NAME};Trusted_Connection=yes")
--- Corona-management-system/models.py
+++ Corona-management-system/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship, declarative_base
 from connect_to_sqlserver import ConnectSQL
 
@@ -18,16 +18,20 @@ class Patient(Base):
     phone: Mapped[str] = mapped_column(String(10))
     cellphone: Mapped[str] = mapped_column(String(10))
     positive_result_date: Mapped[Date] = mapped_column(Date)
-    Recovery_date: Mapped[Date] = mapped_column(Date)
+    recovery_date: Mapped[Date] = mapped_column(Date)
+
+    vaccinations: Mapped["Vaccination"] = relationship(back_populates="patient")
 
 
 class Vaccination(Base):
     __tablename__ = 'vaccinations'
 
-    vaccineID: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    patientID: Mapped[int] = mapped_column(foreign_key=True, autoincrement=True)
+    vaccine_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    patient_id: Mapped[int] = mapped_column(ForeignKey("patients.id"))
     date: Mapped[Date] = mapped_column(Date)
     manufacturer: Mapped[str] = mapped_column(String(30))
+
+    patient: Mapped["Patient"] = relationship(back_populates="vaccinations")
 
 
 Base.metadata.create_all(ConnectSQL.engine)
