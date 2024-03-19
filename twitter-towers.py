44e2ec7b62dcfe879e73ada63999239cebf5b2be
Chagit <chagitalxndr@gmail.com>
2024-03-19 13:13:38 +0200

    Add function: PrintMainMenu() The function prints the visual screen to the user.
    Add function: main()
    The function manages the calling of the functions.

--- twitter-towers.py
+++ twitter-towers.py
@@ -16,14 +16,14 @@ def PrintTriangle(height, width):
     star_temp = 0
     space_temp = 0
 
-    #Print the first line:
+    # Print the first line:
     for i in range(width // 2):
         print(" ", end="")
     print("*")
     star_temp += 1
     space_temp += 1
 
-    #Printing the middle rows:
+    # Printing the middle rows:
     for i in range(rows_in_group + rest_of_rows):
         for j in range(width // 2 - 1):
             print(" ", end="")
@@ -41,9 +41,10 @@ def PrintTriangle(height, width):
                 print("*", end="")
             print()
 
-    #Print the last line:
+    # Print the last line:
     for i in range(width):
         print("*", end="")
+    print()
 
 
 def TriangularTower():
@@ -82,3 +83,44 @@ def TriangularTower():
             PrintTriangle(height, width)
 
 
+def PrintMainMenu():
+    print("-----------------------------")
+    print("WELCOME TO TWITTER")
+    print("Please select the tower type:")
+    print("1. A rectangular tower")
+    print("2. A triangular tower")
+    print("To log out, please press 3")
+    print("-----------------------------")
+
+
+def main():
+    # Menu display:
+    global selection
+    PrintMainMenu()
+
+    # Choosing a type of tower:
+    while True:
+        try:
+            selection = int(input())
+
+            # Checking the integrity of the input:
+            if 0 < selection < 4:
+                if selection == 1:
+                    RectangularTower()
+                    PrintMainMenu()
+                elif selection == 2:
+                    TriangularTower()
+                    PrintMainMenu()
+                else:
+                    print("Thanks for using Twitter!")
+                    break
+            else:
+                print("The value you entered is invalid. Please enter a number between 1 and 3")
+        except ValueError:
+            print("The value you entered is invalid. Please enter a number between 1 and 3")
+
+
+# RectangularTower()
+
+if __name__ == "__main__":
+    main()
