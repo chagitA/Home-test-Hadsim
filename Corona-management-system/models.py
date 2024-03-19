a70d317f281f760298431412b691e86b68a440a7
Chagit <chagitalxndr@gmail.com>
2024-03-19 22:25:50 +0200

    Create class: Vaccination, update Patient class. -Creating Vaccination class and Vaccination table in the database.

--- Corona-management-system/models.py
+++ Corona-management-system/models.py
@@ -13,7 +13,21 @@ class Patient(Base):
     lastname: Mapped[str] = mapped_column(String(30))
     city: Mapped[str] = mapped_column(String(30))
     street: Mapped[str] = mapped_column(String(30))
-    housenum: Mapped[int] = mapped_column()
+    house_num: Mapped[int] = mapped_column()
     birthdate: Mapped[Date] = mapped_column(Date)
     phone: Mapped[str] = mapped_column(String(10))
     cellphone: Mapped[str] = mapped_column(String(10))
+    positive_result_date: Mapped[Date] = mapped_column(Date)
+    Recovery_date: Mapped[Date] = mapped_column(Date)
+
+
+class Vaccination(Base):
+    __tablename__ = 'vaccinations'
+
+    vaccineID: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    patientID: Mapped[int] = mapped_column(foreign_key=True, autoincrement=True)
+    date: Mapped[Date] = mapped_column(Date)
+    manufacturer: Mapped[str] = mapped_column(String(30))
+
+
+Base.metadata.create_all(ConnectSQL.engine)
