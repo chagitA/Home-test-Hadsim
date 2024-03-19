ba6177671daffb78cd38e4ae0d5ff6bfe9b26851
Chagit <chagitalxndr@gmail.com>
2024-03-19 16:44:33 +0200

    Create class: ConnectSQL. The class define the connection definitions to sql.

---
+++ Corona-management-system/connect_to_sqlserver.py
@@ -0,0 +1,22 @@
+import sqlalchemy as db
+import urllib
+from sqlalchemy import text
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+class ConnectSQL:
+    DRIVER_NAME = "SQL SERVER"
+    SERVER_NAME = "DESKTOP-E20A7OU/SQLEXPRESS"
+    DATABASE_NAME = "HMODatabase"
+
+    quoted = urllib.parse.quote_plus(f"DRIVER={DRIVER_NAME};SERVER={SERVER_NAME};DATABASE={DATABASE_NAME};Trusted_Connection=yes")
+    engine = db.create_engine('mssql+pyodbc:///?odbc_connect={}'.format(quoted))
+    conn = engine.connect()
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    Base = declarative_base()
+
+    def __init__(self):
+        pass
