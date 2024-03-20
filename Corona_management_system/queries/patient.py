ef859219ee3251c0b3abaebf98b69c1366cf8ab0
Chagit <chagitalxndr@gmail.com>
2024-03-20 13:53:34 +0200

    Create Patient queries: -creating new_patient function, that create a new patient. -creating get_patient function, that bring the patient by name or id.

---
+++ Corona_management_system/queries/patient.py
@@ -0,0 +1,32 @@
+from sqlalchemy import select
+
+from Corona_management_system.connect_to_sqlserver import ConnectSQL
+from Corona_management_system.models import Patient
+
+Base = ConnectSQL.Base
+session = ConnectSQL.session
+
+
+class Patient_queries:
+    def new_patient(self, n_id, n_first_name, n_last_name,
+                    n_city, n_street, n_house_num,
+                    n_birthday, n_phone, n_cellphone,
+                    n_positive_result_date, n_recovery_date):
+
+        new_patient = Patient(id=n_id, first_name=n_first_name, last_nam=n_last_name,
+                              city=n_city,street=n_street, house_num=n_house_num,
+                              birthday=n_birthday, phone=n_phone, cellphon=n_cellphone,
+                              positive_result_date=n_positive_result_date, recovery_date=n_recovery_date)
+        session.add(new_patient)
+        session.commit()
+
+    def get_patient(self, patient_id):
+        if type(patient_id) == str:
+            stmt = select(Patient).where((Patient.firstname + " " + Patient.lastname) == patient_id)
+        else:
+            stmt = select(Patient).where(Patient.id == patient_id)
+        result = session.execute(stmt)
+
+        for p in result.scalar():
+            return p
+
