7075c1f1c5d3244cc93b35a531f5108384a21fb2
Chagit <chagitalxndr@gmail.com>
2024-03-20 19:34:49 +0200

    -creating new_patient function, that create a new patient. -creating get_patient function, that bring the patient by name or id.

---
+++ Corona_management_system/queries/vaccination.py
@@ -0,0 +1,31 @@
+from sqlalchemy import select
+
+from Corona_management_system.connect_to_sqlserver import ConnectSQL
+from Corona_management_system.models import Vaccination
+
+Base = ConnectSQL.Base
+session = ConnectSQL.session
+
+
+class Vaccination_queries:
+    # A function that sends the newly created patient to the database:
+    def new_vaccine(self, n_vaccine_id, n_patient_id, n_date, n_manufacturer):
+        new_vaccine = Vaccination(vaccin_id=n_vaccine_id, patient_id=n_patient_id,
+                                  date=n_date, manufacturer=n_manufacturer)
+        session.add(new_vaccine)
+        session.commit()
+
+    # A function that accepts a vaccine or a set of vaccines according to an ID:
+    def get_vaccine(self, vaccine_id, type_id):
+        # Sending a set of vaccines in case of a patient's ID:
+        if type(type_id) is "patient_id":
+            stmt = select(Vaccination).where(Vaccination.patient_id == vaccine_id)
+            result = session.execute(stmt)
+            return [v for v in result.scalars()]
+
+        # Sending a single vaccine in case of a vaccine ID:
+        else:
+            stmt = select(Vaccination).where(Vaccination.vaccine_id == vaccine_id)
+            result = session.execute(stmt)
+            for v in result.scalar():
+                return v
