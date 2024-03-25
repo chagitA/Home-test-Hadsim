a085348a6dee1c055736524658cb5bdc698d3bc5
Chagit <chagitalxndr@gmail.com>
2024-03-25 21:45:42 +0200

    Removal of date of receiving a positive result and date of recovery. These dates will receive a null value and can be updated with separate functions.

--- Corona_management_system/queries/patient.py
+++ Corona_management_system/queries/patient.py
@@ -11,12 +11,10 @@ class Patient_queries:
     # A function that sends the newly created patient to the database:
     def new_patient(self, n_id, n_first_name, n_last_name,
                     n_city, n_street, n_house_num,
-                    n_birthday, n_phone, n_cellphone,
-                    n_positive_result_date, n_recovery_date):
+                    n_birthday, n_phone, n_cellphone):
         new_patient = Patient(id=n_id, firstname=n_first_name, lastname=n_last_name,
                               city=n_city, street=n_street, house_num=n_house_num,
-                              birthdate=n_birthday, phone=n_phone, cellphone=n_cellphone,
-                              positive_result_date=n_positive_result_date, recovery_date=n_recovery_date)
+                              birthdate=n_birthday, phone=n_phone, cellphone=n_cellphone)
         session.add(new_patient)
         session.commit()
 
