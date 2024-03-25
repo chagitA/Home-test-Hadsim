c35f0513b215dfd02bdcdab4723e8a464920f16a
Chagit <chagitalxndr@gmail.com>
2024-03-25 22:24:07 +0200

    Adding the function israeli_id_validation() the function checks the correctness of an Israeli identity number

--- Corona_management_system/validation.py
+++ Corona_management_system/validation.py
@@ -1,2 +1,13 @@
+# Function for checking a valid Israeli ID number:
+def israeli_id_validation(id_number):
+    # Ensure the ID number is composed of exactly nine digits
+    if not (id_number.isdigit() and len(id_number) == 9):
+        return False
+
+    # Calculate the sum of the digits according to the ID validation algorithm
+    sum_ = sum(int(digit) * (1 if i % 2 == 0 else 2) % 10 + int(digit) * (1 if i % 2 == 0 else 2) // 10
+               for i, digit in enumerate(id_number))
+    # Check if the sum is divisible by 10
+    return sum_ % 10 == 0
 
 
