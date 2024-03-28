8c609e43503fb91874b4c056d54b9505003f1bb6
Chagit <chagitalxndr@gmail.com>
2024-03-28 16:58:58 +0200

    Patient chart update: -Changing the form of storage in order to allow receiving files. - Adding the vaccination counter to the table.

--- Corona_management_system/models.py
+++ Corona_management_system/models.py
@@ -5,23 +5,38 @@ from connect_to_sqlserver import ConnectSQL
 Base = ConnectSQL.Base
 
 
+
 class Patient(Base):
     __tablename__ = 'patients'
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    firstname: Mapped[str] = mapped_column(String(30))
-    lastname: Mapped[str] = mapped_column(String(30))
-    city: Mapped[str] = mapped_column(String(30))
-    street: Mapped[str] = mapped_column(String(30))
-    house_num: Mapped[int] = mapped_column()
-    birthdate: Mapped[Date] = mapped_column(Date)
-    phone: Mapped[str] = mapped_column(String(10))
-    cellphone: Mapped[str] = mapped_column(String(10))
-    positive_result_date: Mapped[Date] = mapped_column(Date, nullable=True, default=None)
-    recovery_date: Mapped[Date] = mapped_column(Date, nullable=True, default=None)
-    image: Mapped[bytes] = mapped_column(LargeBinary)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    firstname = Column(String(30))
+    lastname = Column(String(30))
+    city = Column(String(30))
+    street = Column(String(30))
+    house_num = Column(Integer)
+    birthdate = Column(Date)
+    phone = Column(String(10))
+    cellphone = Column(String(10))
+    positive_result_date = Column(Date, nullable=True, default=None)
+    recovery_date = Column(Date, nullable=True, default=None)
+    num_of_vaccines = Column(Integer, default=0)
 
-    vaccinations: Mapped["Vaccination"] = relationship(back_populates="patient")
+    image = relationship("Image", uselist=False, back_populates="patient")
+    vaccinations = relationship("Vaccination", back_populates="patient")
+
+
+class Image(Base):
+    __tablename__ = 'images'
+
+    image_id = Column(Integer, primary_key=True, autoincrement=True)
+    patient_id = Column(Integer, ForeignKey("patients.id"))
+    patient = relationship("Patient", back_populates="image")
+    data = Column(String())
+    image_name = Column(String())
+    file_type = Column(String())
+
+
 
 
 class Vaccination(Base):
